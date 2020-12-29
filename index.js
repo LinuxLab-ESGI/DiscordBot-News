c7c0d3f3be203e505ed6c790000e021ed627d61d
Xen0rInspire <nicolas.fouchard94370@gmail.com>
2020-12-29 18:59:58 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { token, organization } = require('./config.json');
+var fs = require('fs'); 
+const { exec } = require("child_process");
+
+client.once('ready', () => {
+
+  console.log('Ready !');
+
+});
+
+// Command exec
+exec("curl -H \"Accept: application / vnd.github.v3 + json\" https://api.github.com/orgs/" + organization + "/repos > repos.json", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
+// read file sample.json file
+fs.readFile('repos.json',
+  // callback function that is called when reading file is done
+  function (err, data) {
+    // json data
+    var jsonData = data;
+
+    // parse json
+    var jsonParsed = JSON.parse(jsonData);
+
+    // access elements
+    console.log(jsonParsed[0].name);
+    console.log(jsonParsed[1].name);
+  }); 
+
+
+client.login(token);
